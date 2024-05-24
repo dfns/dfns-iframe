3dc41431ddcd5862b19006246b25ad462eafe9d4
Rodrigo Cauduro <rod@dfns.co>
2024-05-24 11:46:00 +0200

    feat: update registration flow - simplify

--- examples/basic/app/page.tsx
+++ examples/basic/app/page.tsx
@@ -44,7 +44,6 @@ export default function Home() {
     isConnectReady,
     login,
     logout,
-    loginUserWithToken,
     signTransaction,
     showUserCredentials,
     showIframeScreen,
@@ -52,11 +51,8 @@ export default function Home() {
     errorPayload,
   } = useDfnsConnect(onParentAction);
 
-  const {
-    getRegisterInitChallenge,
-    getRestartRegisterInitChallenge,
-    delegatedLoginNewUser,
-  } = useServerRequests();
+  const { getRegisterInitChallenge, getRestartRegisterInitChallenge } =
+    useServerRequests();
 
   async function createUserWithWallet() {
     try {
@@ -69,12 +65,6 @@ export default function Home() {
         challenge,
         wallets: [newWallet],
       });
-      const { token } = await delegatedLoginNewUser(userName);
-      await loginUserWithToken({
-        token,
-        showScreen: IframeActiveState.waiting,
-      });
-      await showIframeScreen({ showScreen: IframeActiveState.userWallet });
     } catch (e) {
       console.error(e);
     }
@@ -104,153 +94,167 @@ export default function Home() {
 
   useEffect(() => {
     if (!errorPayload) return;
+    // listen for errors returned from iframe
     console.log({ errorPayload });
   }, [errorPayload]);
 
   return (
-    <main className=" min-h-screen bg-[#CCC] text-[black] p-4 max-w-[90hw] flex flex-col ">
-      <p>isDfnsIframeReady: {isConnectReady ? "true" : "false"}</p>
-
-      <button
-        className="bg-black text-white p-4 rounded-lg m-2"
-        onClick={async () => {
-          await showIframeScreen({
-            showScreen: IframeActiveState.recoveryCodes,
-          });
-        }}
-      >
-        Show Recover Codes
-      </button>
-      <button
-        className="bg-black text-white p-4 rounded-lg m-2"
-        onClick={async () => {
-          await logout({ showScreen: IframeActiveState.createUserAndWallet });
-        }}
-      >
-        logout
-      </button>
-      <button
-        className="bg-black text-white p-4 rounded-lg m-2"
-        onClick={async () => await showUserCredentials()}
-      >
-        Show Credentials
-      </button>
-      <label>
-        Username:
-        <input
-          className="p-4 m-3"
-          value={userName}
-          onChange={(e) => {
-            setUserName(e.target.value);
-          }}
-          placeholder="username"
-        />
-        <button
-          className="bg-black text-white p-4 rounded-lg m-2"
-          onClick={async () => {
-            if (!userName) {
-              console.error("userName is not set");
-              return;
-            }
-            await login({ userName, showScreen: IframeActiveState.userWallet });
-          }}
-        >
-          login {userName}
-        </button>
-      </label>
-
-      <button
-        className="bg-black text-white p-4 rounded-lg m-2"
-        onClick={() => {
-          signTransaction({
-            kind: "Eip712",
-            types: {
-              Person: [
-                { name: "name", type: "string" },
-                { name: "wallet", type: "address" },
-              ],
-              Mail: [
-                { name: "from", type: "Person" },
-                { name: "to", type: "Person" },
-                { name: "contents", type: "string" },
-              ],
-            },
-            domain: {
-              name: "Ether Mail",
-              version: "1",
-              chainId: 1,
-              verifyingContract: "0x1b352de7a926ebd1bf52194dab487c2cb0793a9b",
-              salt: "0xf2d857f4a3edcb9b78b4d503bfe733db1e3f6cdc2b7971ee739626c97e86a558",
-            },
-            message: {
-              from: {
-                name: "Chris",
-                wallet: "0x00e3495cf6af59008f22ffaf32d4c92ac33dac47",
-              },
-              to: {
-                name: "Bob",
-                wallet: "0xcc0ee1a1c5e788b61916c8f1c96c960f9a9d3db7",
-              },
-              contents: "Hello, Bob!",
-            },
-          });
-        }}
-      >
-        Sign transaction
-      </button>
-      <button
-        className="bg-black text-white p-4 rounded-lg m-2"
-        onClick={() => {
-          signTransaction({
-            domain: {
-              name: "GRVTEx",
-              version: "0",
-              chainId: 1,
-            },
-            types: {
-              EIP712Domain: [
-                {
-                  name: "name",
-                  type: "string",
-                },
-                {
-                  name: "version",
-                  type: "string",
-                },
-                {
-                  name: "chainId",
-                  type: "uint256",
-                },
-              ],
-              CreateAccount: [
-                {
-                  name: "accountID",
-                  type: "address",
-                },
-                {
-                  name: "nonce",
-                  type: "uint32",
-                },
-              ],
-            },
-            message: {
-              // @ts-expect-error diverges from IEIP712TypedData
-              accountID: "0xf6e6a0d1ebb8d19b432a1680f6c3bbee8fb2d6fe",
-              nonce: "1781961150",
-            },
-            kind: "Eip712",
-          });
-        }}
-      >
-        Sign transaction 2
-      </button>
-      <h3 className="mt-16 mb-2">Dfns Iframe</h3>
-      <div className="border-8 border-sky-500 w-[505px]">
+    <main className="min-h-screen bg-[#CCC] text-[black] p-4 max-w-[90hw] flex flex-row gap-8">
+      <div className="border-8 border-sky-500 w-[505px] h-[495px]">
         <DfnsIframe
           initialScreen={IframeActiveState.createUserAndWallet}
           iframeWidth={490}
           iframeHeight={480}
         />
+      </div>
+      <div className="flex flex-col">
+        <p>isDfnsIframeReady: {isConnectReady ? "true" : "false"}</p>
+        <button
+          className="bg-black text-white p-4 rounded-lg m-2"
+          onClick={async () => {
+            await showIframeScreen({
+              showScreen: IframeActiveState.recoveryCodes,
+            });
+          }}
+        >
+          Show Recover Codes
+        </button>
+        <button
+          className="bg-black text-white p-4 rounded-lg m-2"
+          onClick={async () => {
+            await showIframeScreen({
+              showScreen: IframeActiveState.recover,
+            });
+          }}
+        >
+          Recover
+        </button>
+        <button
+          className="bg-black text-white p-4 rounded-lg m-2"
+          onClick={async () => {
+            await logout({ showScreen: IframeActiveState.createUserAndWallet });
+          }}
+        >
+          logout
+        </button>
+        <button
+          className="bg-black text-white p-4 rounded-lg m-2"
+          onClick={async () => await showUserCredentials()}
+        >
+          Show Credentials
+        </button>
+        <label>
+          Username:
+          <input
+            className="p-4 m-3"
+            value={userName}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            placeholder="username"
+          />
+          <button
+            className="bg-black text-white p-4 rounded-lg m-2"
+            onClick={async () => {
+              if (!userName) {
+                console.error("userName is not set");
+                return;
+              }
+              await login({
+                userName,
+                showScreen: IframeActiveState.userWallet,
+              });
+            }}
+          >
+            login {userName}
+          </button>
+        </label>
+
+        <button
+          className="bg-black text-white p-4 rounded-lg m-2"
+          onClick={() => {
+            signTransaction({
+              kind: "Eip712",
+              types: {
+                Person: [
+                  { name: "name", type: "string" },
+                  { name: "wallet", type: "address" },
+                ],
+                Mail: [
+                  { name: "from", type: "Person" },
+                  { name: "to", type: "Person" },
+                  { name: "contents", type: "string" },
+                ],
+              },
+              domain: {
+                name: "Ether Mail",
+                version: "1",
+                chainId: 1,
+                verifyingContract: "0x1b352de7a926ebd1bf52194dab487c2cb0793a9b",
+                salt: "0xf2d857f4a3edcb9b78b4d503bfe733db1e3f6cdc2b7971ee739626c97e86a558",
+              },
+              message: {
+                from: {
+                  name: "Chris",
+                  wallet: "0x00e3495cf6af59008f22ffaf32d4c92ac33dac47",
+                },
+                to: {
+                  name: "Bob",
+                  wallet: "0xcc0ee1a1c5e788b61916c8f1c96c960f9a9d3db7",
+                },
+                contents: "Hello, Bob!",
+              },
+            });
+          }}
+        >
+          Sign transaction
+        </button>
+        <button
+          className="bg-black text-white p-4 rounded-lg m-2"
+          onClick={() => {
+            signTransaction({
+              domain: {
+                name: "GRVTEx",
+                version: "0",
+                chainId: 1,
+              },
+              types: {
+                EIP712Domain: [
+                  {
+                    name: "name",
+                    type: "string",
+                  },
+                  {
+                    name: "version",
+                    type: "string",
+                  },
+                  {
+                    name: "chainId",
+                    type: "uint256",
+                  },
+                ],
+                CreateAccount: [
+                  {
+                    name: "accountID",
+                    type: "address",
+                  },
+                  {
+                    name: "nonce",
+                    type: "uint32",
+                  },
+                ],
+              },
+              message: {
+                // @ts-expect-error diverges from IEIP712TypedData
+                accountID: "0xf6e6a0d1ebb8d19b432a1680f6c3bbee8fb2d6fe",
+                nonce: "1781961150",
+              },
+              kind: "Eip712",
+            });
+          }}
+        >
+          Sign transaction 2
+        </button>
       </div>
     </main>
   );
